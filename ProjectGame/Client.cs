db7c6adde06a5210b14b8cce79459c83fa8ec885
samfromdeath <samuel@intelogy.com.au>
2020-11-07 10:45:29 +1100

    Add project files.

---
+++ ProjectGame/Client.cs
@@ -0,0 +1,15 @@
+﻿using OpenTK.Windowing.Desktop;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectGame
+{
+    public class Client : GameWindow
+    {
+        public Client(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings) : base(gameWindowSettings, nativeWindowSettings)
+        {
+
+        }
+    }
+}
---
+++ ProjectGame/Program.cs
@@ -0,0 +1,26 @@
+﻿using OpenTK.Windowing.Common;
+using System;
+
+namespace ProjectGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var client = new Client(new OpenTK.Windowing.Desktop.GameWindowSettings()
+            {
+                IsMultiThreaded = true,
+                RenderFrequency = 60,
+                UpdateFrequency = 60
+            },
+            new OpenTK.Windowing.Desktop.NativeWindowSettings()
+            {
+                IsFullscreen =  true,
+                API = ContextAPI.OpenGL,
+                APIVersion = new Version(4, 1)
+            });
+
+            client.Run();            
+        }
+    }
+}
